5f84a4d90d8f7c6f8884fb24376510341a07a6ff
Михаил <mikhailnov@dumalogiya.ru>
2017-09-30 15:44:19 +0300

    Rename from trimage to optimgaika

--- setup.py
+++ setup.py
@@ -4,31 +4,31 @@ import sys
 win=(sys.platform == "win32")
 if win:
   import py2exe
-  sys.path.append("src/trimage")
+  sys.path.append("src/optimgaika")
 
 from distutils.core import setup
 
-setup(name = "trimage",
+setup(name = "optimgaika",
     version = "1.0.2",
-    description = "Trimage image compressor - A cross-platform tool for optimizing PNG and JPG files",
+    description = "Optimgaika image compressor - A cross-platform tool for optimizing PNG and JPG files",
     author = "Kilian Valkhof, Paul Chaplin",
-    author_email = "help@trimage.org",
-    url = "http://trimage.org",
+    author_email = "help@optimgaika.org",
+    url = "http://optimgaika.org",
     license = "MIT license",
-    package_dir = {'trimage' : 'src/trimage'},
-    packages = ["trimage",
-              "trimage.filesize",
-              "trimage.ThreadPool",],
-    package_data = {"trimage" : ["pixmaps/*.*"] },
-    data_files=[('share/icons/hicolor/scalable/apps', ['desktop/trimage.svg']),
-            ('share/applications', ['desktop/trimage.desktop']),
-            ('share/man/man1', ['doc/trimage.1'])],
-    scripts = ["trimage"],
-    long_description = """Trimage is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and guetzli, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Trimage gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
+    package_dir = {'optimgaika' : 'src/optimgaika'},
+    packages = ["optimgaika",
+              "optimgaika.filesize",
+              "optimgaika.ThreadPool",],
+    package_data = {"optimgaika" : ["pixmaps/*.*"] },
+    data_files=[('share/icons/hicolor/scalable/apps', ['desktop/optimgaika.svg']),
+            ('share/applications', ['desktop/optimgaika.desktop']),
+            ('share/man/man1', ['doc/optimgaika.1'])],
+    scripts = ["optimgaika"],
+    long_description = """Optimgaika is a cross-platform GUI and command-line interface to optimize image files via optipng, advpng and guetzli, depending on the filetype (currently, PNG and JPG files are supported). It was inspired by imageoptim. All image files are losslessy compressed on the highest available compression levels. Optimgaika gives you various input functions to fit your own workflow: A regular file dialog, dragging and dropping and various command line options.""",
     requires = ["PyQt4 (>=4.4)"],
 
     #for py2exe
-    windows=[r'src\trimage\trimage.py'],
+    windows=[r'src\optimgaika\optimgaika.py'],
     zipfile=None,
     options={"py2exe":{
         "optimize":2,
